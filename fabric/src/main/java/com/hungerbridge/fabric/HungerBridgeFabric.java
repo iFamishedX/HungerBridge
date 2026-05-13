af864677ab05f350628e13490ea2c880dc1ba5dc
iFamishedX <me@ifamished.com>
2026-05-13 23:49:44 +0000

    fix

--- fabric/src/main/java/com/hungerbridge/fabric/HungerBridgeFabric.java
+++ fabric/src/main/java/com/hungerbridge/fabric/HungerBridgeFabric.java
@@ -1,5 +1,18 @@
-@Override
-public void onInitialize() {
-    Platform.setAdapter(new FabricPlatformAdapter());
-    LOGGER.info("HungerBridge Fabric mod initialized");
+package com.hungerbridge.fabric;
+
+import com.hungerbridge.common.util.Platform;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HungerBridgeFabric implements ModInitializer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("HungerBridge");
+
+    @Override
+    public void onInitialize() {
+        Platform.setAdapter(new FabricPlatformAdapter());
+
+        LOGGER.info("HungerBridge Fabric mod initialized");
+    }
 }
