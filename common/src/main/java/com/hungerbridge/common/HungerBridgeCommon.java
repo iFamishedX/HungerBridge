3d84a2b57c518d4b3191910d6280f1fe19bbf8e7
iFamishedX <me@ifamished.com>
2026-05-14 00:53:44 +0000

    fix

--- common/src/main/java/com/hungerbridge/common/HungerBridgeCommon.java
+++ common/src/main/java/com/hungerbridge/common/HungerBridgeCommon.java
@@ -2,32 +2,40 @@ package com.hungerbridge.common;
 
 import com.hungerbridge.common.util.Platform;
 
+import java.nio.file.Path;
+
 public class HungerBridgeCommon {
 
-    private static Server httpServer;
+    private Server httpServer;
 
-    public static void onServerStart(Object platformServer) {
+    public void start(Object server) {
         try {
-            var adapter = Platform.getAdapter();
-            var server = adapter.unwrap(platformServer);
+            // Adapter must already be set by Paper/Fabric
+            var adapter = Platform.adapter();
 
-            var cfgDir = adapter.getConfigDir(server);
-            var config = Config.load(cfgDir);
+            Path cfgDir = adapter.getConfigDir(server);
+            Config config = Config.load(cfgDir);
 
+            // Initialize executor + logger from adapter
             Platform.init(
                     adapter.getCommandExecutor(server),
-                    Platform::log
+                    adapter.getLogger()
             );
 
             httpServer = new Server(config);
             httpServer.start();
 
+            Platform.logger().log("info",
+                    "HungerBridge HTTP server started on port " + config.port);
+
         } catch (Exception e) {
-            Platform.log("error", "Failed to start HungerBridge: " + e.getMessage());
+            Platform.logger().log("error",
+                    "Failed to start HungerBridge: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public static void onServerStop() {
+    public void stop() {
         if (httpServer != null) {
             httpServer.stop();
         }
