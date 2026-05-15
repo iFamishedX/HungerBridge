81e19e208d604fa51a08f0ee4c7dfffc4d7bdb0e
iFamishedX <me@ifamished.com>
2026-05-15 16:05:02 +0000

    Fix

--- fabric/src/main/java/com/hungerbridge/fabric/mixin/MinecraftServerLogMixin.java
+++ fabric/src/main/java/com/hungerbridge/fabric/mixin/MinecraftServerLogMixin.java
@@ -2,20 +2,20 @@ package com.hungerbridge.fabric.mixin;
 
 import com.hungerbridge.fabric.OutputCapture;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(DedicatedServer.class)
-public abstract class DedicatedServerMixin {
+@Mixin(MinecraftServer.class)
+public abstract class MinecraftServerLogMixin {
 
     @Inject(method = "logMessage", at = @At("HEAD"), cancellable = true)
-    private void hungerbridge$interceptConsoleOutput(Component message, CallbackInfo ci) {
+    private void hungerbridge$interceptLogMessage(Component message, CallbackInfo ci) {
         if (OutputCapture.isActive()) {
             OutputCapture.add(message.getString());
-            ci.cancel();
+            ci.cancel(); // suppress console output
         }
     }
 }
